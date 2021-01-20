7feeddeb0b341a02aecbe471827c635f8f1f6ed0
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-20 22:05:58 +0000

    API integration complete

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const btn = document.getElementById('btn');
+
 async function getWeather() {
     const response = await fetch(
-        'api.openweathermap.org/data/2.5/weather?q=London&appid=b559fa679543062582bf71f3041919d1',
+        'https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=b559fa679543062582bf71f3041919d1',
         { mode: 'cors' }
     );
     const weatherData = await response.json();
-    console.log(weatherData.weather.main);
+    console.log(weatherData.name);
+    console.log(weatherData.sys.country);
+    console.log(weatherData.weather[0].main);
 }
 
-getWeather();
+btn.addEventListener('click', getWeather)
