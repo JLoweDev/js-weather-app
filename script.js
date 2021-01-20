6b3f2ef2cdf99c08cdcb145bcfcacb06165b17b3
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-20 22:17:04 +0000

    units (metric/imperial) and city are now variables

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+let units = 'metric';
+let city = 'London';
+
 async function getWeather() {
     const response = await fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=London&appid=b559fa679543062582bf71f3041919d1',
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=b559fa679543062582bf71f3041919d1`,
         { mode: 'cors' }
     );
     const weatherData = await response.json();
     console.log(weatherData.name);
     console.log(weatherData.sys.country);
     console.log(weatherData.weather[0].main);
+    console.log(weatherData.main.temp);
 }
 
 getWeather();
